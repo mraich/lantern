4f9def4c45e0dd2f6d688b9e6b1d13b49563c6ab
Mester Richárd <mrichard@citromail.hu>
2020-05-31 20:10:10 +0200

    Dice:: A dobókocka működésének kezdetei.
     Jelenleg a dobás esetét írtam le.

--- scripts/dice.gd
+++ scripts/dice.gd
@@ -1,3 +1,31 @@
 # script: dice
 
 extends Node2D
+
+# This is our current value.
+var value
+
+# These are the minimal and maximal possible values.
+const MIN = 1
+const MAX = 6
+
+# Indicating the value has changed by rolling.
+signal on_value_changed_by_roll
+
+func _ready():
+	randomize()
+
+	roll()
+	pass
+
+# Generating new value for the dice.
+func generate_new_value():
+	value = randi() % MAX + MIN
+	pass
+
+# Rolling the dice.
+func roll():
+	generate_new_value()
+
+	emit_signal("on_value_changed_by_roll", self)
+	pass
