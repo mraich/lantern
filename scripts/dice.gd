559775b88957f1137af7369e77565cc8c43ef3a0
Mester Richárd <mrichard@citromail.hu>
2020-05-31 23:08:25 +0200

    Dice:: A kód átrendezése.

--- scripts/dice.gd
+++ scripts/dice.gd
@@ -24,16 +24,16 @@ func _ready():
 	generate_new_value()
 	pass
 
-# Generating new value for the dice.
-func generate_new_value():
-	value = randi() % MAX + MIN
-	pass
-
 # Rolling the dice.
 func roll():
 	generate_new_value()
 
 	emit_signal("on_value_changed", self)
+	pass
+
+# Generating new value for the dice.
+func generate_new_value():
+	value = randi() % MAX + MIN
 	pass
 
 # Critical hit.
