5e4718402e09fc0197d7288099f8f9b9905143e6
Mester Richárd <mrichard@citromail.hu>
2020-06-01 16:22:46 +0200

    DiceContainer:: Egy dobókocka értékének változásának eseményét továbbküldjük.

--- scripts/dice_container.gd
+++ scripts/dice_container.gd
@@ -5,11 +5,21 @@ extends HBoxContainer
 const _DICE_COUNT = 6
 const _dice_class = preload("res://scenes/dice.tscn")
 
+# Indicating the value has changed.
+signal on_value_changed
+
 func _ready():
 	for n in range(0, _DICE_COUNT):
 		var dice = _dice_class.instance()
 
 		# Passing through events.
+		# Passing through the signal for a dice value changing.
+		dice.connect("on_value_changed", self, "_on_dice_value_changed")
 
 		add_child(dice)
+	pass
+
+func _on_dice_value_changed(var dice):
+	# Listeners will know that this dice value has been changed.
+	emit_signal("on_value_changed", dice)
 	pass
