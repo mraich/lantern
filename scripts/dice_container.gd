1c9422dfefe0a5bbf48a5e4d4e9f68948db7adc2
Mester Richárd <mrichard@citromail.hu>
2020-06-01 15:53:23 +0200

    DiceContainer:: Valamennyi kocka létrehozása.

--- scripts/dice_container.gd
+++ scripts/dice_container.gd
@@ -2,5 +2,11 @@
 
 extends HBoxContainer
 
+const _DICE_COUNT = 6
+const _dice_class = preload("res://scenes/dice.tscn")
+
 func _ready():
+	for n in range(0, _DICE_COUNT):
+		var dice = _dice_class.instance()
+		add_child(dice)
 	pass
