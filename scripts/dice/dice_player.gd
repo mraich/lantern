3171e599925babd50fbc719b2bb7e8f2eb1d9eb3
Mester Richárd <mrichard@citromail.hu>
2020-07-20 21:14:16 +0200

    DicePlayer:: A megfordíthatóság és a hozzá tartozó művelet bevezetése.

--- scripts/dice/dice_player.gd
+++ scripts/dice/dice_player.gd
@@ -1,3 +1,20 @@
 # script: dice_player
 
 extends "res://scripts/dice/dice_rollable.gd"
+
+# Tells if this dice can be flipped.
+var is_flippable
+
+func _ready():
+	set_flippable(true)
+	pass
+
+func set_flippable(is_flippable):
+	self.is_flippable = is_flippable
+	pass
+
+func flip():
+	if is_flippable:
+		var new_value = _MAX + _MIN - _value
+		_set_value(new_value)
+	pass
