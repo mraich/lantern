a6b61dbf8ea3339515acd23c353e628c2ddf4c94
Mester Richárd <mrichard@citromail.hu>
2020-07-21 19:49:18 +0200

    DicePlayer:: Az újradobhatóság és a hozzá tartozó művelet bevezetése.

--- scripts/dice/dice_player.gd
+++ scripts/dice/dice_player.gd
@@ -5,16 +5,29 @@ extends "res://scripts/dice/dice_rollable.gd"
 # Tells if this dice can be flipped.
 var is_flippable
 
+# Tells if this dice can be rolled.
+var is_rollable
+
 func _ready():
 	set_flippable(true)
+	set_rollable(true)
 	pass
 
 func set_flippable(is_flippable):
 	self.is_flippable = is_flippable
 	pass
 
+func set_rollable(is_rollable):
+	self.is_rollable = is_rollable
+	pass
+
 func flip():
 	if is_flippable:
 		var new_value = _MAX + _MIN - _value
 		_set_value(new_value)
+	pass
+
+func roll():
+	if is_rollable:
+		.roll()
 	pass
