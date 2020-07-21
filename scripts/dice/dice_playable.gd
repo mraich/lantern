b235432f5e11e58d501451c3507047954ff88f62
Mester Richárd <mrichard@citromail.hu>
2020-07-21 20:55:57 +0200

    DicePlayable:: A dobókocka értékének növelése, és csökkentése 1-el.

--- scripts/dice/dice_playable.gd
+++ scripts/dice/dice_playable.gd
@@ -8,9 +8,18 @@ var is_flippable
 # Tells if this dice can be rolled.
 var is_rollable
 
+# Tells if this dice can be alterable.
+# Altering means we can increase or decrease the value by 1.
+var is_alterable
+
 func _ready():
+	set_alterable(true)
 	set_flippable(true)
 	set_rollable(true)
+	pass
+
+func set_alterable(is_alterable):
+	self.is_alterable = is_alterable
 	pass
 
 func set_flippable(is_flippable):
@@ -19,6 +28,23 @@ func set_flippable(is_flippable):
 
 func set_rollable(is_rollable):
 	self.is_rollable = is_rollable
+	pass
+
+func inc():
+	_alter(1)
+	pass
+
+func dec():
+	_alter(-1)
+	pass
+
+func _alter(alter_by):
+	if is_alterable:
+		var new_value = _value + alter_by
+		if new_value >= _get_min_value() and new_value <= _get_max_value():
+			_set_value(new_value)
+			pass
+		pass
 	pass
 
 func flip():
