1a167f839c014fa1f5c53a843983ca0ffe262419
Mester Richárd <mrichard@citromail.hu>
2020-07-21 20:44:47 +0200

    DiceRollable:: Az újonnan generált érték először egy külön változóba kerül.

--- scripts/dice/dice_rollable.gd
+++ scripts/dice/dice_rollable.gd
@@ -12,7 +12,8 @@ func _ready():
 
 # Generating new value for the dice.
 func generate_new_value():
-	_set_value(random.randi_range(_get_min_value(), _get_max_value()))
+	var new_value = random.randi_range(_get_min_value(), _get_max_value())
+	_set_value(new_value)
 	pass
 
 # Rolling the dice.
