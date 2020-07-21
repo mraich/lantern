dabaae5e063bfe51b543faea5f6ff55c35e5b151
Mester Richárd <mrichard@citromail.hu>
2020-07-21 20:31:36 +0200

    DiceRollable:: Egy RandomNumberGenerator objektum használata.

--- scripts/dice/dice_rollable.gd
+++ scripts/dice/dice_rollable.gd
@@ -2,15 +2,17 @@
 
 extends "res://scripts/dice/dice_base.gd"
 
+var random = RandomNumberGenerator.new()
+
 func _ready():
-	randomize()
+	random.randomize()
 
 	generate_new_value()
 	pass
 
 # Generating new value for the dice.
 func generate_new_value():
-	_set_value(randi() % _get_max_value() + _get_min_value())
+	_set_value(random.randi_range(_get_min_value(), _get_max_value()))
 	pass
 
 # Rolling the dice.
