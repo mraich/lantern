22853f6e9206fb9b56ceecaeaeb01750cfa0f716
Mester Richárd <mrichard@citromail.hu>
2020-07-26 14:39:03 +0200

    DiceContainer:: Az összes kocka z_index-ének a kezelése a z_index script-nek a segítségével.

--- scripts/container/dice_container.gd
+++ scripts/container/dice_container.gd
@@ -2,6 +2,9 @@
 
 extends HBoxContainer
 
+var t_z_index = preload("res://scripts/resource/z_index.gd")
+var z_index = t_z_index.new()
+
 var dices = []
 const _DICE_COUNT = 6
 const _dice_class = preload("res://scenes/dice/dice_playable.tscn")
@@ -26,10 +29,15 @@ func _ready():
 		dice.connect("on_value_changed", self, "_on_dice_value_changed")
 
 		add_child(dice)
+
+	# These are the objects we want to maintain z index.
+	z_index._set_objects(dices)
 	pass
 
 # Dragging of the dice.
 func _on_drag_begin(dice):
+	# Change the z index to max for the dice we are dragging.
+	z_index._change_z_index_to_max(dice)
 	pass
 
 func _on_dice_value_changed(var dice):
