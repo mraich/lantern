f386f4a2fae3144cbc216d47ea18c075a0d5cd25
Mester Richárd <mrichard@citromail.hu>
2020-07-26 22:05:39 +0200

    DiceContainer:: Az on_all_dices_rolled signal törlése.

--- scripts/container/dice_container.gd
+++ scripts/container/dice_container.gd
@@ -12,9 +12,6 @@ const _dice_class = preload("res://scenes/dice/dice_playable.tscn")
 # Indicating the value has changed.
 signal on_value_changed
 
-# Indicating the all dice function is called.
-signal on_all_dices_rolled
-
 func _ready():
 	for n in range(0, _DICE_COUNT):
 		var dice = _dice_class.instance()
@@ -63,7 +60,4 @@ func _on_dice_value_changed(var dice):
 func roll_all():
 	for dice in dices:
 		dice.roll()
-
-	# Notifying the outside world that the function is completed.
-	emit_signal("on_all_dices_rolled")
 	pass
