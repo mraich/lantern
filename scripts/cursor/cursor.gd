71a4ca54ac8c3be76af6b7afe340fa39cfd7ad31
Mester Richárd <mrichard@citromail.hu>
2020-10-07 20:08:41 +0200

    Cursor:: A Cursor hozzáadása.
      Ez majd az a mutató lesz, amivel el lehet majd az egyes műveleteket
      végezni az adott dobókockákon.

---
+++ scripts/cursor/cursor.gd
@@ -0,0 +1,11 @@
+# script: cursor
+
+extends Node2D
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	pass
+
+func _process(delta):
+	self.position = self.get_global_mouse_position()
+	pass
