235c09ef13eda7c037107a86923636b4ecfae0ee
Mester Richárd <mrichard@citromail.hu>
2020-10-08 20:32:20 +0200

    World:: A cursor alapállapotának a beállítása.

--- scripts/world.gd
+++ scripts/world.gd
@@ -8,6 +8,9 @@ onready var cursor = get_node("cursor")
 func _ready():
 	# Connecting the state change of the cursor.
 	cursor.connect("on_cursor_state_changed", self, "_on_cursor_state_changed")
+
+	# Initial state of the cursor.
+	cursor.set_state(0)
 	pass
 
 # On cursor state change.
