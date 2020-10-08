d688fa7220ae0d7d03995f32a5c04736c9249f32
Mester Richárd <mrichard@citromail.hu>
2020-10-08 20:46:06 +0200

    World:: A dice_container tárolása egy változóban.

--- scripts/world.gd
+++ scripts/world.gd
@@ -5,6 +5,9 @@ extends Node2D
 # Finding the cursor in the tree of the world.
 onready var cursor = get_node("cursor")
 
+# Finding the dice container in the tree of the world.
+onready var dice_container = get_node("dice_container")
+
 func _ready():
 	# Connecting the state change of the cursor.
 	cursor.connect("on_cursor_state_changed", self, "_on_cursor_state_changed")
