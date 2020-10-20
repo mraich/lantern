a7d1ecac782b4dee388c5d44e7ef6537244c28d7
Mester Richárd <mrichard@citromail.hu>
2020-10-20 20:59:51 +0200

    DiceContainer:: Az általánosítás kezdetei. Egy külön init függvénybe kerülnek a kockák létrehozásának a kódjai.

--- scripts/container/dice_container.gd
+++ scripts/container/dice_container.gd
@@ -3,16 +3,17 @@
 extends HBoxContainer
 
 var dices = []
-const _DICE_COUNT = 6
-const _dice_class = preload("res://scenes/dice/dice_playable.tscn")
 
 # Indicating the value has changed.
 signal on_value_changed
 
 func _ready():
+	pass
+
+func init(dice_class, dice_count):
 	var next_position = 0
-	for n in range(0, _DICE_COUNT):
-		var dice = _dice_class.instance()
+	for n in range(0, dice_count):
+		var dice = dice_class.instance()
 		dice.position = Vector2(next_position, dice.get_height())
 		next_position += dice.get_width()
 		dices.push_back(dice)
@@ -26,7 +27,7 @@ func _ready():
 	# Correcting the position of the dices to have them
 	# to be arranged in the center of the given position
 	# for the DiceContainer.
-	var offsetX = -(next_position - next_position / _DICE_COUNT) / 2
+	var offsetX = -(next_position - next_position / dice_count) / 2
 	for dice in dices:
 		dice.position.x += offsetX
 	pass
