b204cc8344eac351e143e68f2c035078df31f064
Mester Richárd <mrichard@citromail.hu>
2020-10-24 18:49:18 +0200

    DiceClickable:: Egy felesleges üres sor eltávolítása.

--- scripts/dice/dice_clickable.gd
+++ scripts/dice/dice_clickable.gd
@@ -5,7 +5,6 @@ extends "res://scripts/dice/dice_rollable.gd"
 func _ready():
 	# Setting signal for recognize clicking on the dice area.
 	get_node("dice_area").connect("on_dice_clicked", self, "_on_dice_clicked")
-
 	pass
 
 func _on_dice_clicked():
