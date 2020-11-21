0f8d1f0c7943c1f4707819c6c18e943f43b71cee
Mester Richárd <mrichard@citromail.hu>
2020-11-21 14:07:56 +0100

    Dice:: A gyerek node-ok elérésének egy egyszerűbb módja.

--- scripts/dice/dice.gd
+++ scripts/dice/dice.gd
@@ -68,7 +68,7 @@ func _set_value(new_value):
 func _show_value():
 	# Showing the right picture if present.
 	if _dice_face != null:
-		get_node("face_sprite").texture = _dice_face
+		$face_sprite.texture = _dice_face
 	pass
 
 func get_value():
