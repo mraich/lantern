50affc2e4bdece6ecba20ec9beceb3513a80f022
Mester Richárd <mrichard@citromail.hu>
2020-11-25 20:07:47 +0100

    StageManager:: A StageManager scene kezdetei.

---
+++ scripts/algorithm/stage_manager.gd
@@ -0,0 +1,29 @@
+# script: stage_manager
+
+extends Node
+
+var stage = 1
+
+# Puzzle variable.
+onready var puzzle = [1, 2, 3, 4, 5, 6]
+
+# Emitted when we loaded a new stage.
+signal on_stage_load
+
+func on_game_start():
+	stage = 1
+	_load_stage()
+	pass
+
+func on_stage_win():
+	stage += 1
+	_load_stage()
+	pass
+
+func on_stage_lose():
+	on_game_start()
+	pass
+
+func _load_stage():
+	emit_signal("on_stage_load", puzzle)
+	pass
