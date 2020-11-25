d99b55d42e9ee5f36844b748a711845ea04f6a68
Mester Richárd <mrichard@citromail.hu>
2020-11-25 21:19:24 +0100

    World:: A StageManager használatának a kezdetei.

--- scripts/world.gd
+++ scripts/world.gd
@@ -16,15 +16,18 @@ func _ready():
 	# removed when the stage handling is ready.
 	# The playable container should show this count of dices.
 	$playable_container.set_dice_count(6)
-	# The puzzle container should show this arrangement.
-	var arrangement = [1, 2, 3, 4, -1, -2]
-	$puzzle_container.set_arrangement(arrangement)
 
 	# Getting notified about the changes made on the playable field.
 	$playable_container.connect("on_value_changed", self, "on_playable_value_changed")
 
 	# Getting notified about the changes made on the puzzle field.
 	$puzzle_container.connect("on_value_changed", self, "on_puzzle_value_changed")
+
+	# Getting notified about the loading of the stages.
+	$stage_manager.connect("on_stage_load", self, "_on_stage_load")
+
+	# Game starts.
+	$stage_manager.on_game_start()
 	pass
 
 func _input(event):
@@ -33,12 +36,24 @@ func _input(event):
 		$cursor.step_state()
 	pass
 
+# On loading the next stage.
+func _on_stage_load(puzzle):
+	# New set of values for the dices.
+	$playable_container.roll_all()
+
+	# The puzzle container should show this arrangement.
+	$puzzle_container.set_arrangement(puzzle)
+	pass
+
 # On cursor state change.
 func _on_cursor_state_changed(state):
 	$playable_container.set_dice_action(state)
 	pass
 
 func on_playable_value_changed():
+	if $dice_checker.check($playable_container.get_values(), $puzzle_container.get_values()):
+		$stage_manager.on_stage_win()
+		pass
 	pass
 
 func on_puzzle_value_changed():
