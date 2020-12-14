e5e7fefb99d925e432c4f158a3fe8c2b222fe5f4
Mester Richárd <mrichard@citromail.hu>
2020-12-14 23:20:08 +0100

    Character:: A kinézet, és az animáció megadása.

--- scripts/character/character.gd
+++ scripts/character/character.gd
@@ -3,4 +3,8 @@
 extends Node2D
 
 func _ready():
+	# Changing the appearance of the hero.
+	$sprite.set_texture(load("res://res/character/hero_001.png"))
+	# Choosing the animation.
+	$animation.play("walk_right")
 	pass
