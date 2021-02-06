8739c958176df6610ea1ac98b6db856d228d9149
Mester Richárd <mrichard@citromail.hu>
2021-02-06 09:30:28 +0100

    Camera:: A camera scene bevezetése.

---
+++ scripts/camera.gd
@@ -0,0 +1,19 @@
+# script: camera
+
+extends Camera2D
+
+var scene_followed = null
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	if scene_followed != null:
+		var position = scene_followed.get_position()
+		set_position(position)
+		pass
+	pass
+
+func get_total_pos():
+	return position + offset
+	pass
