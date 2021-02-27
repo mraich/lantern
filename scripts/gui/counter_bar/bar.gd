ceba1d50bdfb761762523c852d3c775c3c9ebdf6
Mester Richárd <mrichard@citromail.hu>
2021-02-27 18:07:13 +0100

    Bar:: A Bar bevezetése.

---
+++ scripts/gui/counter_bar/bar.gd
@@ -0,0 +1,21 @@
+# script:: bar
+
+extends Node2D
+
+func _ready():
+	switch(false)
+	pass
+
+func switch(is_on):
+	$sprite_on.visible = is_on
+	$sprite_off.visible = not is_on
+	pass
+
+func get_width():
+	return 10 * scale.x
+
+func get_height():
+	return 10 * scale.y
+
+func is_on():
+	return $sprite_on.visible
