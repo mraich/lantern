eb1f8b4f28e646f616fc5a65ae1a0c7d8a0952ad
Mester Richárd <mrichard@citromail.hu>
2021-03-12 20:40:02 +0100

    Camera:: A működésre vonatkozó megjegyzések felvétele.

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -2,8 +2,10 @@
 
 extends Camera2D
 
+# We can follow multiple scenes.
 var scenes_followed = []
 
+# This is the scene we follow at the moment.
 var scene_followed = null
 
 func _ready():
@@ -11,10 +13,12 @@ func _ready():
 
 func _physics_process(delta):
 	if scene_followed != null and weakref(scene_followed).get_ref():
+		# When we have a nice usable scene to follow.
 		var position = scene_followed.get_position()
 		set_position(position)
 		pass
 	else:
+		# If the scene we follow becomes null or gets deleted then we must follow the last scene we followed before.
 		scene_followed = scenes_followed.pop_back()
 		pass
 	pass
@@ -23,16 +27,19 @@ func get_total_pos():
 	return position + offset
 	pass
 
+# Set follow only this scene.
 func set_follow(to_follow):
 	scenes_followed = []
 	scene_followed = to_follow
 	pass
 
+# Follow this scene but remember the last scene.
 func push_follow(to_follow):
 	scenes_followed.push_back(scene_followed)
 	scene_followed = to_follow
 	pass
 
+# Don't follow and don't remember this scene.
 func not_follow(to_not_follow):
 	if scene_followed == to_not_follow:
 		scene_followed = null
