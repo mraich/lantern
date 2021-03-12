6c592c81efdd5772f39921ee01a234fa747c9f7d
Mester Richárd <mrichard@citromail.hu>
2021-03-12 20:23:47 +0100

    Camera:: Lehetőség egymás után több Scene követésére a set_follow, a push_follow, és a not_follow függvények segítségével.

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -2,6 +2,8 @@
 
 extends Camera2D
 
+var scenes_followed = []
+
 var scene_followed = null
 
 func _ready():
@@ -12,8 +14,32 @@ func _physics_process(delta):
 		var position = scene_followed.get_position()
 		set_position(position)
 		pass
+	else:
+		scene_followed = scenes_followed.pop_back()
+		pass
 	pass
 
 func get_total_pos():
 	return position + offset
+	pass
+
+func set_follow(to_follow):
+	scenes_followed = []
+	scene_followed = to_follow
+	pass
+
+func push_follow(to_follow):
+	scenes_followed.push_back(scene_followed)
+	scene_followed = to_follow
+	pass
+
+func not_follow(to_not_follow):
+	if scene_followed == to_not_follow:
+		scene_followed = null
+
+	var i = 0
+	for scene in scenes_followed:
+		if (scene == to_not_follow):
+			scenes_followed.remove(i)
+		i += 1
 	pass
